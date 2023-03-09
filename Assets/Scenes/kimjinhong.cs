62f222779f878276327cb9f9af6d00e238b0afd6
kimjinhong2023 <kt01098550133@gmail.com>
2023-03-09 19:13:39 +0900

    테스트

---
+++ Assets/Scenes/kimjinhong.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kimjinhong : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Log");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
