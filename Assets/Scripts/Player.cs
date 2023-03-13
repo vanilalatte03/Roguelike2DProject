aabfb5591b596bc259feb33d8971bb72112993ac
kimjinhong2023 <kt01098550133@gmail.com>
2023-03-13 21:40:16 +0900

    플레이어 원거리 공격 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     public float speed;
+
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    public float coolTime;
+    private float currentTime;
+
+    void Start()
+    {
+        
+    }
+
     void Update()
     { 
         if (Input.GetKey(KeyCode.D))
@@ -17,9 +27,24 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
             transform.Translate(-Vector2.up * speed);
         // 이거 Switch 문으로 못바꾸나?
+        // 내가 Project Setting에서 Horizontal이랑 Vertical 설정 바꿔놔서
+        // 이동도 그냥 GetAxis로 Horizontal/Vertical 사용해서 구현해도 될 것 같아
 
-      
+        float shootH = Input.GetAxisRaw("ShootH");
+        float shootV = Input.GetAxisRaw("ShootV");
+        if((shootH != 0 || shootV != 0) && currentTime <= 0)
+        {
+            Shoot(shootH, shootV);
 
+            currentTime = coolTime;
+        }
+        currentTime -= Time.deltaTime;
+    }
+    
+    void Shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(x, y) * bulletSpeed;
     }
 }
         
