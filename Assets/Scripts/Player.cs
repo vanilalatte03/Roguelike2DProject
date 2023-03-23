1de5b99d6b6b706f4f0cf2c46698f1ab2fa04491
kimjinhong2023 <kt01098550133@gmail.com>
2023-03-23 15:33:57 +0900

    강의실 때려잡기

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,8 @@ public class Player : MonoBehaviour
     public float bulletSpeed;
     public float coolTime;
     private float currentTime;
+    private int direction = 1;
+    private int shootDir = -45;
 
     void Start()
     {
@@ -18,25 +20,21 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        /*
-        if (Input.GetKey(KeyCode.D))
-            transform.Translate(Vector2.right * speed);
-        if (Input.GetKey(KeyCode.A))
-            transform.Translate(-Vector2.right * speed);
-        if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector2.up * speed);
-        if (Input.GetKey(KeyCode.S))
-            transform.Translate(-Vector2.up * speed);
-        */
-
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
+        if (h < 0) direction = -1;
+        else if (h > 0) direction = 1;
         Vector2 CurPos = transform.position;
         Vector2 ChaPos = new Vector2(h, v) * speed * Time.deltaTime;
+        transform.localScale = new Vector3(direction, 1, 1);
         transform.position = CurPos + ChaPos;
 
         float shootH = Input.GetAxisRaw("ShootH");
         float shootV = Input.GetAxisRaw("ShootV");
+        if (shootH < 0) shootDir = 135;
+        else if (shootH > 0) shootDir = -45;
+        else if (shootV < 0) shootDir = -135;
+        else if (shootV > 0) shootDir = 45;
         if((shootH != 0 || shootV != 0) && currentTime <= 0)
         {
             Shoot(shootH, shootV);
@@ -50,7 +48,7 @@ public class Player : MonoBehaviour
     {
         if (x != 0) y = 0;
 
-        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.Euler(new Vector3(0, 0, shootDir)));
         bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(x, y) * bulletSpeed;
     }
 }
