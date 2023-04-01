9fdcacceeb30522ba67b9bd39cd94995d1985b03
EldenRingGod <0wodnjs0823@naver.com>
2023-04-01 21:17:39 +0900

    조이스틱 구현.

---
+++ Assets/Scripts/JoyStick.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    [SerializeField] private RectTransform rect_Background;
+    [SerializeField] private RectTransform rect_JoyStick; // 원이 따라나오면 안되니깐 사각형에 가둬놓는 구문. 
+    [SerializeField] private GameObject go_Player;
+    [SerializeField] private float MoveSpeed;
+
+    private bool isTouch = false;
+    private Vector3 movePosition;
+
+    private float radius;
+    void Start()
+    {
+        radius = rect_Background.rect.width * 0.5f;
+    }
+
+    void Update()
+    {
+        if (isTouch)
+        {
+            go_Player.transform.position += movePosition;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) // 눌렀을 때 
+    {
+        isTouch = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData) // 뗐을 때 
+    {
+        isTouch = false; 
+        rect_JoyStick.localPosition = Vector3.zero;
+
+    }
+
+    public void OnDrag(PointerEventData eventData) // 드래그 할 때 
+    {
+        Vector2 value = eventData.position - (Vector2)rect_Background.position;
+        // position은 x,y축만 존재하기에 vector2이지만 rect.position은 vector3 이기에 강제변환. 
+        value = Vector2.ClampMagnitude(value, radius);
+        // 가둬두기 구문. 
+        rect_JoyStick.localPosition = value;
+
+        value = value.normalized;
+        movePosition = new Vector3(value.x * MoveSpeed * Time.deltaTime, value.y * MoveSpeed * Time.deltaTime, 0f);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
     Rigidbody2D rigidbody;
+
 
     public GameObject bulletPrefab;
     public float bulletSpeed;
@@ -22,12 +23,13 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
+       /* float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical"); // 수직 및 수평으로 이동하는 걸 받아옴. 
         if (h < 0) direction = -1;
         else if (h > 0) direction = 1;
   
-        rigidbody.velocity = new Vector3(h * speed, v * speed, 0);// 캐릭터 이동 구문. 
+        rigidbody.velocity = new Vector3(h * speed, v * speed, 0);// 캐릭터 이동 구문. */
+
         transform.localScale = new Vector3(direction, 1, 1); // 스프라이트 회전
    
         float shootH = Input.GetAxisRaw("ShootH");
@@ -52,4 +54,6 @@ public class Player : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.Euler(new Vector3(0, 0, shootDir)));
         bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(x, y) * bulletSpeed;
     }
+
+    
 }
