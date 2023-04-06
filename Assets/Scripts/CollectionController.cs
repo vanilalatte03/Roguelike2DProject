ed6f8b9acbb729a99ded0e365c51a5004d9b59ac
kimjinhong2023 <kt01098550133@gmail.com>
2023-04-06 11:59:34 +0900

    Potion 충돌 안되게

--- Assets/Scripts/CollectionController.cs
+++ Assets/Scripts/CollectionController.cs
@@ -22,6 +22,7 @@ public class CollectionController : MonoBehaviour
         GetComponent<SpriteRenderer>().sprite = item.itemImage;
         Destroy(GetComponent<PolygonCollider2D>());
         gameObject.AddComponent<PolygonCollider2D>();
+        gameObject.GetComponent<PolygonCollider2D>().isTrigger = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
