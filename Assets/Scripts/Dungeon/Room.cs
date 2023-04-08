6e4bfa2d66381402583efaf566d2381bf0a58457
EldenRingGod <0wodnjs0823@naver.com>
2023-04-08 10:35:47 +0900

    수정 1

--- Assets/Scripts/Dungeon/Room.cs
+++ Assets/Scripts/Dungeon/Room.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int Width;
+
+    public int Height;
+
+    public int X;
+
+    public int Y; 
     void Start()
     {
-        
+        if(RoomController.instance == null)
+        {
+            Debug.Log("잘못된 씬입니다.");
+            return;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()  
     {
-        
+        // 기즈모는 scene에서 게임 오브젝트와 관련된 그래픽, 시각적인 디버깅을 위해 본 구문이 사용
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height, 0));
+
     }
+
+    public Vector3 GetRoomCenter()
+    {
+        return new Vector3(X * Width, Y * Height);
+    }
+
 }
--- Assets/Scripts/Dungeon/RoomController.cs
+++ Assets/Scripts/Dungeon/RoomController.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+public class RoomInfo
+{
+    public string name;
+
+    public int X;
+
+    public int Y; 
+    // START 지점이 (0,0)이라고 할 때, 오른쪽으로 가면 (1,0) 위로 가면 (0,1) 이런 식으로 구현하기 위함.
+}
 public class RoomController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static RoomController instance;
+
+    string currentWorldName = "Basement";
+
+    RoomInfo currentLoadRoomData;
+
+    Queue<RoomInfo> IoadRoomQueue = new Queue<RoomInfo>();
+    // 선입선출 구조 
+
+    public List<Room> loadedRooms = new List<Room>();
+
+    bool isLoadingRoom = false;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+       public bool DoesRoomExist(int x, int y)
     {
-        
+        return loadedRooms.Find(item => item.X == x && item.Y == y) != null;
     }
 }
