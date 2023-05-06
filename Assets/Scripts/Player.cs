7398514be3e554be2a19cf23927db688651fb1dd
EldenRingGod <0wodnjs0823@naver.com>
2023-05-06 21:30:55 +0900

    조이스틱 대거 수정 및 스프라이트 좌우 반전

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
+
 
 public class Player : MonoBehaviour
 {
-    void Start()
+
+    [SerializeField] [Range(1f, 10f)] float barSpeed = 3f;
+    public VariableJoystick joy;
+    public float speed;
+    SpriteRenderer rend;
+
+    Rigidbody2D rb;
+    Vector2 moveVec;
+
+    private void Start()
     {
-       
+        rend = GetComponent<SpriteRenderer>();
+    }
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
+        float x = joy.Horizontal;
+        float y = joy.Vertical;
 
+        if (x >0)
+        {
+            rend.flipX = false;
+        }
+
+        else if (x <0)
+        {
+            rend.flipX = true;
+        }
+
+        moveVec = new Vector2(x, y) * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + moveVec);
     }
 }
