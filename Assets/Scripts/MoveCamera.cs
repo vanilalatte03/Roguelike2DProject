e89c17bedfd7e8a35e6d8afe6e031e0f5b0c48eb
kimjinhong2023 <kt01098550133@gmail.com>
2023-05-23 12:32:12 +0900

    camera 업데이트

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform target;
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+    }
+}
