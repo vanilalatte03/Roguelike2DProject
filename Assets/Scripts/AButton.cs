4414c50fbe88e582d31434a8ce40769d32bc090b
eeennsu <116697733+eeennsu@users.noreply.github.com>
2023-09-06 21:00:37 +0900

    키보드 이동 & 공격 구현
    플레이어 이동 속도 약간 조정 (실제 속도는 이전과 동일)

--- Assets/Scripts/AButton.cs
+++ Assets/Scripts/AButton.cs
@@ -13,24 +13,63 @@ public class AButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public float coolTime;
     private float currentTime;
     private bool isTouch;
+    private bool isApressed;
 
     void Start()
     {
         isTouch = false;
+        isApressed = false;
     }
 
     void Update()
     {
-        coolTime = GameController.FireRate;
+        isApressed = Input.GetKeyDown(KeyCode.A);
 
-        if (isTouch && currentTime <= 0)
+        coolTime = GameController.FireRate;
+        
+        // (모바일의 공격 UI 클릭 또는 키보드의 a키가 눌려거나) && 쿨타임이 0일 때 공격
+        // 하지만 키보드 a 공격은 아직 수정 필요
+        if ((isTouch || isApressed) && currentTime <= 0)
         {
-            Vector2 playerDir = joyStick.GetComponent<VariableJoystick>().AttackDir;
+            // 보강 필요
+            // 모바일 터치일 때는 조이스틱을 가져오고, 키보드 입력일 때는 플레이어의 direction을 가져온다.
+            /*      Vector2 playerDir = new Vector2();
+                  float angle = 0;
+
+                  if (isTouch)
+                  {
+                      playerDir = joyStick.GetComponent<VariableJoystick>().AttackDir;
+                      angle = Mathf.Atan2(playerDir.y, playerDir.x) * Mathf.Rad2Deg;
+                  }
+
+                  else if (isApressed)
+                  {
+                      playerDir = player.GetComponent<Player>().transform.rotation.eulerAngles;
+                      // 보강 필요
+                      // angle = 
+                  }*/
+
+
+
             //x,y의 값을 조합하여 Z방향 값으로 변형함. -> ~도 단위로 변형
-            float angle = Mathf.Atan2(playerDir.y, playerDir.x) * Mathf.Rad2Deg;
-            GameObject bullet = Instantiate(bulletPrefab, player.transform.position, Quaternion.Euler(0, 0, angle - 45));
-            bullet.GetComponent<Rigidbody2D>().velocity = playerDir * bulletSpeed;
-            currentTime = coolTime;
+            if (isTouch)
+            {
+                Vector2 playerDir = joyStick.GetComponent<VariableJoystick>().AttackDir;
+                float angle = Mathf.Atan2(playerDir.y, playerDir.x) * Mathf.Rad2Deg;
+                GameObject bullet = Instantiate(bulletPrefab, player.transform.position, Quaternion.Euler(0, 0, angle - 45));
+                bullet.GetComponent<Rigidbody2D>().velocity = playerDir * bulletSpeed;
+                currentTime = coolTime;
+            }
+
+            // 키보드 a눌렀을 때의 전용 화살
+            else if (isApressed)
+            {
+                Vector2 playerDir = player.GetComponent<Player>().resultVec;
+                float angle = Mathf.Atan2(playerDir.y, playerDir.x) * Mathf.Rad2Deg;
+                GameObject bullet = Instantiate(bulletPrefab, player.transform.position, Quaternion.Euler(0, 0, angle - 45));
+                bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)) * bulletSpeed;
+                currentTime = coolTime;
+            }
         }
         currentTime -= Time.deltaTime;
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,9 @@ public class Player : MonoBehaviour
     SpriteRenderer rend;
 
     Rigidbody2D rb;
-    Vector2 moveVec;
+
+    [HideInInspector]
+    public Vector2 resultVec;           //     기존의 Vector2 moveVec; 변수 대체
 
     private void Start()
     {
@@ -30,20 +32,52 @@ public class Player : MonoBehaviour
             Debug.Log("Heelo");
         }
 
-        float x = joy.Horizontal;
-        float y = joy.Vertical;
+        // 기존 코드
+        /* float x = joy.Horizontal;
+         float y = joy.Vertical;
 
-        if (x >0)
+         if (x >0)
+         {
+             rend.flipX = false;
+         }
+
+         else if (x <0)
+         {
+             rend.flipX = true;
+         }
+
+         moveVec = new Vector2(x, y) * speed * Time.deltaTime;
+         rb.MovePosition(rb.position + moveVec);*/
+
+        // 조이스틱으로 받은 입력값들
+        float joyX = joy.Horizontal;
+        float joyY = joy.Vertical;
+
+        // 키보드로 받은 입력값들
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        // 케릭터 좌우 전환
+        if (joyX > 0 || inputX > 0)
         {
             rend.flipX = false;
         }
 
-        else if (x <0)
+        else if (joyX < 0 || inputX < 0)
         {
             rend.flipX = true;
         }
 
-        moveVec = new Vector2(x, y) * speed * Time.deltaTime;
-        rb.MovePosition(rb.position + moveVec);
+        // 전역변수 resultVec에, 조이스틱으로 움직일 때는 조이스틱 방향으로, 키보드 입력일 때는 키보드 방향으로 움직임
+        resultVec = (joyX != 0 || joyY != 0) ? new Vector2(joyX, joyY) : new Vector2(inputX, inputY); 
+    }
+
+    // 물리 움직임은 Update가 아닌 FixedUpdate에서 해야 안정적.
+    // Update는 프레임이 높아질 수록 더 빠르게 호출됨.
+    // FixedUpdate는 고정된 시간 간격으로 실행되므로 물리적 이동 로직에 안정적임.
+    private void FixedUpdate()
+    {
+        resultVec = resultVec.normalized * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + resultVec); 
     }
 }
