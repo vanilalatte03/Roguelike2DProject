0f89bd67b21ce8dbf6ef7df942de6e2eccc17827
eeennsu <diso592@naver.com>
2023-10-15 12:10:46 +0900

    배경음, 효과음 볼륨 조절 및 저장 추가

--- Assets/Scripts/UI/SoundManager.cs
+++ Assets/Scripts/UI/SoundManager.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Sound
@@ -25,6 +24,21 @@ public class SoundManager : MonoBehaviour
     public Sound[] effectSounds;
     public Sound[] bgmSounds;
 
+    [SerializeField]
+    private float maxAudioSourceVolume = 0.4f;
+
+    [SerializeField]
+    private int maxSliderValue = 10;
+
+    [SerializeField]
+    private Slider bgmSlider;
+
+    [SerializeField]
+    private Slider effectSlider;
+
+    private const string ppBGMVolume = "BGMVolume";
+    private const string ppBGMEffect = "EffectVolume";
+
     private void Awake()
     {
         if (instance == null)
@@ -36,13 +50,41 @@ public class SoundManager : MonoBehaviour
     private void Start()
     {
         playSoundNames = new string[effectSources.Length];
+
+        float bgmVloume = PlayerPrefs.GetFloat(ppBGMVolume, -1f);
+        float effectVolume = PlayerPrefs.GetFloat(ppBGMEffect, -1f);
+
+        // 설정이 없으면 7, 있으면 그대로
+        bgmSlider.value = bgmVloume == -1 ? 7 : bgmVloume;
+        effectSlider.value = effectVolume == -1 ? 7 : effectVolume;
+    }
+
+    public void SetBGMVolume(float volume)
+    {
+        for (int i = 0; i < bgmsSources.Length; i++)
+        {
+            bgmsSources[0].volume = (volume / maxSliderValue) * maxAudioSourceVolume;      // 원래 배경음악의 최대치는 0.4이다.
+        }
+
+        PlayerPrefs.SetFloat(ppBGMVolume, volume);
+    }
+
+
+    public void SetEffectVolume(float volume)
+    {
+        for (int i = 0; i < effectSources.Length; i++)
+        {
+            effectSources[0].volume = (volume / maxSliderValue) * maxAudioSourceVolume;      // 원래 효과음의 최대치는 0.4이다.
+        }
+
+        PlayerPrefs.SetFloat(ppBGMEffect, volume);
     }
 
     // Sound 메뉴 버튼 클릭
-    public void ToggleBGM()
-    {
-        bgmsSources[0].enabled = !bgmsSources[0].enabled;
-    }
+    /*    public void ToggleBGM()
+        {
+            bgmsSources[0].enabled = !bgmsSources[0].enabled;
+        }*/
 
     // 외부에서 호출하는 이펙트 사운드
     public void PlaySoundEffect(string _name)
@@ -54,7 +96,7 @@ public class SoundManager : MonoBehaviour
             if (_name == effectSounds[i].name)
             {
                 // 재생 중이지 않은 오디오 소스를 선택해 재생
-                for (int j = 0; j < effectSources.Length; j++) 
+                for (int j = 0; j < effectSources.Length; j++)
                 {
                     if (!effectSources[j].isPlaying)
                     {
@@ -70,7 +112,7 @@ public class SoundManager : MonoBehaviour
 
                 Debug.Log("모든 오디오 소스가 사용 중이다. 추가를 권장");
                 return;
-            }   
+            }
         }
 
         Debug.Log(_name + "는 등록된 사운드가 아님");
